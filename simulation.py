8a15f08a1fd220c817e2b392e94787f69e636a5d
Nicolò Fusi <nicolo.fusi@gmail.com>
2012-09-11 14:38:43 -0700

    first complete sim

--- simulation.py
+++ simulation.py
@@ -3,6 +3,7 @@ import scipy as sp
 import pdb, sys, pickle
 import matplotlib.pylab as plt
 import datetime
+import scipy.stats
 
 class GWASsim(object):
 
@@ -17,7 +18,7 @@ class GWASsim(object):
         self.MAF = MAF
         self.Fst = Fst
         self.num_causal = num_causal
-        if perc_causal_differentiated*num_causal > num_differentiated:
+        if pop_struct and perc_causal_differentiated*num_causal > num_differentiated:
             print "WARNING! the number of differentiated SNPs is not enough to achieve the desired number of diff. causal SNPs, setting it automatically"
             self.num_differentiated = int(perc_causal_differentiated*num_causal)
             
@@ -28,6 +29,9 @@ class GWASsim(object):
         else:
             self.chr_copies = 1
 
+        if not pop_struct and num_differentiated != 0:
+            self.num_differentiated = 0 
+            
         # Phenotype transformations
         self.pheno_transform = pheno_transform
         self.transform_param = transform_param
@@ -62,13 +66,17 @@ class GWASsim(object):
 
         for k in self.Z.keys():
             self.Y += self.Z[k]
-
+            print "Variance due to %s = %.3f" % (k, self.Z[k].var())
+            
         if self.pheno_transform != None:
             if self.pheno_transform == "exp_ay":
-                self.Y_transformed = np.exp(self.transform_param * self.Y)
+                self.Y_transformed = np.zeros_like(self.Y)
+                self.Y_transformed[:self.N/5.0] = np.exp(self.transform_param * self.Y[:self.N/5.0])
             elif self.pheno_transform == "exp_root":
                 self.Y_transformed = np.exp(self.Y)**(1.0/self.transform_param)
-            
+            elif self.pheno_transform == "rounding":
+                self.Y_transformed = self.Y.copy().round(self.transform_param)
+                
     def generate_names(self):
         """
         Generates SNP/sample/phenotype names. The causal SNPs have "causal_" prepended 
@@ -105,13 +113,16 @@ class GWASsim(object):
             self.differentiated = np.append(self.differentiated, np.random.permutation(non_causal)[:remaining])
         
         self.X = np.zeros((self.N, self.Q))
+
+
+        # self.X = 1.0*(sp.rand(self.N,self.Q)>self.MAF)
         
         for i in range(self.Q):
-            p = np.random.uniform(self.MAF, 0.5)
+            p = np.random.uniform(self.MAF, 1-self.MAF)
             self.X[:,i] = np.random.binomial(self.chr_copies, p, size = self.N)
             # completely superfluous given the p above, but good 
             # for the sanity of mind
-            assert (self.X[:,i] != 0).sum()/float(self.N) > self.MAF
+            # assert (self.X[:,i] != 0).sum()/float(self.N) > self.MAF
 
         # if there's no population structure to be added we are done, otherwise add it.
         if self.pop_struct:
@@ -155,10 +166,17 @@ class GWASsim(object):
     def add_associations(self):
         print "Simulating associations..."
 
-        W = np.zeros((self.Q, self.D))
-        W[self.causal] = np.random.randn(self.num_causal,self.D) * np.sqrt(self.genetic_var)
-
-        self.Z['associations'] = np.dot(self.X, W)
+        if 0:
+            mean = np.array([0.0 for i in range(self.N)])
+            K = np.dot(self.X[:, self.causal], self.X[:, self.causal].T)
+            sigma_g = self.genetic_var/np.diag(K).var()
+            XW = np.random.multivariate_normal(mean, sigma_g*K, self.D).T
+        else:
+            W = np.zeros((self.Q, self.D))
+            W[self.causal] = sp.stats.t.rvs(3, 0.0, 0.1, size=(self.num_causal,1))
+            XW = np.dot(self.X, W)
+            
+        self.Z['associations'] = XW
 
     def add_epistatic_interactions():
         raise NotImplementedError
@@ -182,16 +200,67 @@ class GWASsim(object):
             
         
 
+def boxcox(Y):
+    Y = Y.copy()
+    # Y = Y.flatten()
+
+    Yt = sp.stats.boxcox(Y + np.abs(Y.min()) + 0.01)[0]
+    Yt -= Yt.mean()
+    Yt /= Yt.std()
+
+    return Yt
+
+    
 if __name__ == '__main__':
-    sim = GWASsim(500, 5000, 100, num_differentiated = 1000, genetic_var = 0.1, pop_struct = True, pheno_transform = "exp_ay")
+    sim = GWASsim(2000, 6000, 500, num_differentiated = 5900, genetic_var = 1.0, noise_var = 0.5, pop_struct = True, 
+                  Fst = 0.1, diploid = True, add_assoc = True, add_noise = True, MAF = 0.25, pheno_transform = "exp_root",
+        transform_param = 0.5)
     print sim
     from panama.core import testing
-    K = np.dot(sim.X, sim.X.T)
-    # K = np.diag([1.0 for i in range(sim.N)])
-    covs = np.ones([sim.N,1])
-    pv = testing.interface(sim.X, sim.Y, K, covs, I = None, return_fields=['pv'],
-                           parallel = False, jobs = 0, add_mean = False, delta_opt_params = None,
-                           Ftest = True)[0]
 
+    X = sim.X.copy()
+    Y = sim.Y.copy()
+    Y -= Y.mean()
+    Y /= Y.std()
+    X -= X.mean(axis=0)
+    X /= X.std(axis=0)
+
+    K = np.cov(sim.X[:, sim.differentiated])
+    covs = np.zeros([sim.N,1])
+    pv_ideal = testing.interface(X.copy(), Y.copy(), K, covs, I = None, return_fields=['pv'],
+                           parallel = False, jobs = 0, add_mean = True, delta_opt_params = None,
+                           Ftest = False)[0]
+
+    K = np.diag([1.0 for i in range(sim.N)])
+    pv_linear = testing.interface(X, Y, K, covs, I = None, return_fields=['pv'],
+                                  parallel = False, jobs = 0, add_mean = True, delta_opt_params = None,
+                                  Ftest = False)[0]
+
+
+    K = np.cov(sim.X)#[:, sim.differentiated])
+    pv_bc_kern = testing.interface(X.copy(), boxcox(sim.Y_transformed), K, covs, I = None, return_fields=['pv'],
+                                   parallel = False, jobs = 0, add_mean = True, delta_opt_params = None,
+                                   Ftest = False)[0]
+
+
+    
+    
+    truth = [1.0 if i.find("causal") != -1 else 0.0 for i in sim.snps_names]
+    import sklearn.metrics as metrics
     plt.figure()
-    plt.hist(pv.flatten(), bins = 30)
+    fpr, tpr, thrs = metrics.roc_curve(truth, -pv_ideal.flatten())
+    plt.plot(fpr, tpr, label = "Ideal (kernel + untransformed pheno)")
+    fpr, tpr, thrs = metrics.roc_curve(truth, -pv_linear.flatten())
+    plt.plot(fpr, tpr, label = "Linear regression (no kernel, transformed pheno)")
+    fpr, tpr, thrs = metrics.roc_curve(truth, -pv_bc_kern.flatten())
+    plt.plot(fpr, tpr, label = "BoxCox (kernel)")
+    plt.legend(loc=0)
+    # plt.xlim((0.0, 0.2))
+
+    # from panama.utilities import qq
+    # from panama.utilities.fdr import estimate_lambda
+    # qq.qq_plot(pv_ideal.flatten())
+    # qq.qq_plot(pv_linear.flatten())    
+    # print "\n\n"
+    # print "Kernel", estimate_lambda(pv_ideal.flatten())
+    # print "NO Kernel", estimate_lambda(pv_linear.flatten())
